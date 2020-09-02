6a9a7677a9228dfd9c29b15bdd0b7362dabd3ec8
khalladay <k.mj.halladay@gmail.com>
2020-09-01 21:28:31 -0500

    it's ugly as hell, but the embedded disasm example is working. needs to handle calls and needs a major cleanup, but WOOHOO

--- hooking-by-example/trampoline-embedded-disasm-same-process/trampoline-embedded-disasm-same-process.cpp
+++ hooking-by-example/trampoline-embedded-disasm-same-process/trampoline-embedded-disasm-same-process.cpp
@@ -28,14 +28,117 @@ struct HookDesc
 	void* trampolineMem;
 	void* longJumpMem;
 
-	cs_insn* stolenInstructions;
 	uint8_t stolenInstructionCount;
 	uint8_t stolenInstructionSize;
 };
 
 extern "C" void call_hook_payload();
 
+#pragma optimize("", off)
+//converts relative instructions to absolute ones
+uint32_t BuildStolenByteBuffer(HookDesc* hook, uint8_t* outBuffer, uint8_t** outGapPtr, uint32_t outBufferSize)
+{
+	// Disassemble stolen bytes
+	csh handle;
+	cs_err dis_err = cs_open(CS_ARCH_X86, CS_MODE_64, &handle);
+	check(dis_err == CS_ERR_OK);
 
+	size_t count;
+	cs_insn* disassembledInstructions;
+	count = cs_disasm(handle, (uint8_t*)hook->originalFunc, 20, (uint64_t)hook->originalFunc, 20, &disassembledInstructions);
+	check(count > 0);
+
+	//get the instructions covered by the first 5 bytes of the original function
+	uint32_t numBytes = 0;
+	uint32_t numOriginBytes = 0;
+	std::vector<std::pair<uint32_t, uint32_t>> jumps;
+	std::vector<std::pair<uint32_t, uint32_t>> calls;
+	uint32_t numInstructions = 0;
+
+	for (int i = 0; i < count; ++i)
+	{
+		cs_insn inst = disassembledInstructions[i];
+
+		//all condition jumps are relative, as are all E9 jmps. non-E9 "jmp" is absolute, so no need to deal with it
+		bool isRelJump = inst.id >= X86_INS_JAE && inst.id <= X86_INS_JS;
+		if (inst.id == X86_INS_JMP && inst.bytes[0] != 0xE9) isRelJump = false;
+		if (isRelJump)
+		{
+			jumps.push_back({ i,numOriginBytes });
+			numBytes += 12;
+		}
+		else if (inst.id == X86_INS_CALL)
+		{
+			calls.push_back({ i, numOriginBytes });
+			numBytes += 12;
+		}
+		numOriginBytes += inst.size;
+		numBytes += inst.size;
+		numInstructions++;
+		if (numOriginBytes >= 5) break;
+	}
+	*outGapPtr = &outBuffer[numOriginBytes];
+	hook->stolenInstructionSize = numOriginBytes;
+	numOriginBytes += 12; //enough for the jmp back to the original function 
+	numBytes += 12;
+	//relative jumps need to be converted to absolute, 64 bit jumps
+	//but since those require the destruction of a register, I'm going
+	//to move that logic to the end of the stolen bytes (in case previous
+	//instructions use that register). Rather than try to determine if each 
+	//jump is relative or not, ALL jumps will go through the jump table
+	
+	//so in assembly, this is going to look like this: 
+	/*		Old            |               NEW            
+	 ----------------------|-----------------------------
+		jmp 32			   |		jmp 2
+		nop				   |		nop
+		nop				   |		nop
+						   |		mov rax, <Original Jmp Target>
+						   |		jmp rax
+	*/
+
+	uint32_t jumpTablePos = numOriginBytes;
+
+	for (auto jmp : jumps)
+	{
+		cs_insn& instr = disassembledInstructions[jmp.first];
+		char* jmpTargetAddr = instr.op_str;
+		uint8_t distToJumpTable = jumpTablePos - (jmp.second + instr.size);
+		
+		//all 2 byte jump opcodes start with 0f
+		//if 2 bytes, the operand is larger than a uint8 ... so TODO
+		uint8_t* operand = instr.bytes[0] == 0x0F ? &instr.bytes[2] : &instr.bytes[1];
+		*operand = distToJumpTable;
+
+		uint8_t jmpBytes[] = { 0x48, 0xB8, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, //movabs into rax
+								0xFF, 0xE0 }; //jmp rax
+		uint64_t targetAddr = _strtoui64(jmpTargetAddr, NULL, 0);
+		memcpy(&jmpBytes[2], &targetAddr, 8);
+		memcpy(&outBuffer[jumpTablePos], jmpBytes, sizeof(jmpBytes));
+		jumpTablePos += 12;
+			
+	}
+
+	for (auto call : calls)
+	{
+		char* callOperand = disassembledInstructions[call.first].op_str;
+		uint32_t distToCallTable = jumpTablePos - call.second;
+		jumpTablePos += 12;
+	}
+
+	//similarly, all calls will be converted to relative jumps into a call table
+
+	uint32_t writePos = 0;
+	for (int i = 0; i < numInstructions; ++i)
+	{
+		cs_insn inst = disassembledInstructions[i];
+		memcpy(&outBuffer[writePos], inst.bytes, inst.size);
+		writePos += inst.size;
+	}
+	cs_close(&handle);
+
+	return numBytes;
+}
 
 void InstallHook(HookDesc* hook)
 {
@@ -43,33 +146,9 @@ void InstallHook(HookDesc* hook)
 	bool err = VirtualProtect(hook->originalFunc, 1024, PAGE_EXECUTE_READWRITE, &oldProtect);
 	check(err);
 
-	// Disassemble stolen bytes
-	csh handle;
-	cs_err dis_err = cs_open(CS_ARCH_X86, CS_MODE_64, &handle);
-	check(dis_err == CS_ERR_OK);
-
-	size_t count;
-	count = cs_disasm(handle, (uint8_t*)hook->originalFunc, 20, (uint64_t)hook->originalFunc, 20, &hook->stolenInstructions);
-	check(count > 0);
-
-	for (int i = 0; i < count; ++i)
-	{
-		cs_insn inst = hook->stolenInstructions[i];
-		hook->stolenInstructionSize += inst.size;
-		hook->stolenInstructionCount++;
-		if (hook->stolenInstructionSize >= 5) break;
-	}
-	check(hook->stolenInstructionSize >= 5);
-	cs_close(&handle);
-
-	//build stolen bytes
-	uint8_t* stolenBytes = (uint8_t*)malloc(hook->stolenInstructionSize);
-	uint8_t* stolenByteIter = stolenBytes;
-	for (int i = 0; i < hook->stolenInstructionCount; ++i)
-	{
-		memcpy(stolenByteIter, hook->stolenInstructions[i].bytes, hook->stolenInstructions[i].size);
-		stolenByteIter += hook->stolenInstructions[i].size;
-	}
+	uint8_t stolenBytes[1024];
+	uint8_t* jmpBackLoc = nullptr;
+	uint32_t numStolenBytes = BuildStolenByteBuffer(hook, stolenBytes, &jmpBackLoc, 1024);
 
 	uint8_t callAsmBytes[] = {	0x48, 0xB8, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA, //movabs 64 bit value into rax
 								0xFF, 0xD0, //call rax
@@ -96,11 +175,12 @@ void InstallHook(HookDesc* hook)
 	uint64_t hookPayloadAddr = (uint64_t)call_hook_payload;
 	memcpy(trampolineBytePtr, &callAsmBytes, sizeof(callAsmBytes));
 	trampolineBytePtr += sizeof(callAsmBytes);
-	memcpy(trampolineBytePtr, stolenBytes, hook->stolenInstructionSize);
-	trampolineBytePtr += hook->stolenInstructionSize;
-	memcpy(trampolineBytePtr, jmpBytes, sizeof(jmpBytes));
-	free(stolenBytes);
 
+	memcpy(jmpBackLoc, jmpBytes, sizeof(jmpBytes));
+	
+	memcpy(trampolineBytePtr, stolenBytes, numStolenBytes);
+	trampolineBytePtr += numStolenBytes;
+	
 	//write jumps
 	WriteAbsoluteJump64(hook->longJumpMem, hook->trampolineMem);
 	WriteRelativeJump(hook->originalFunc, hook->longJumpMem, hook->stolenInstructionSize - 5);
@@ -120,6 +200,10 @@ int main(int argc, const char** argv)
 	InstallHook(&hook);
 
 	TargetFunc(argc-1, (float)argc);
+	TargetFunc(argc, (float)argc);
+	TargetFunc(argc+1, (float)argc);
+
 
 }
 
+#pragma optimize("", on)
